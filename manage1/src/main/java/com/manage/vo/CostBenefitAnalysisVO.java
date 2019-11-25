702c5796175441cb71b89ccb582741a90973a89c
TY <emonety01@gmail.com>
2019-11-25 17:06:18 +0900

    CostBenefitAnalysisVO

---
+++ manage1/src/main/java/com/manage/vo/CostBenefitAnalysisVO.java
@@ -0,0 +1,18 @@
+package com.manage.vo;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class CostBenefitAnalysisVO {
+
+	private String oppId, projectName, userNum, endUser, customer, autoExtension;
+	private int version, profitSort, expectedSales, expectedPurchase, expectedProfit, exchangeRate;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date contractDate, expiredDate;
+	
+}
